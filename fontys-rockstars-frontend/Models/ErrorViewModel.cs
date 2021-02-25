50f8d831c24143c5cb3dedf8202b30221e45a8fa
Connor Timmerman <457736@student.fontys.nl>
2021-02-25 14:29:37 +0100

    Add project files.

---
+++ fontys-rockstars-frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fontys_rockstars_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
